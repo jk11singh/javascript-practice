a102736cad9ffc64b949d8c1a3a90805275cc5e3
jk11singh <147373138+jk11singh@users.noreply.github.com>
2023-10-09 10:06:27 +0000

    variable nd datatypes in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accountEmail = "jaskaran@google.com"
+var accountPassword = "12345"
+accountCity = "Chandigarh"
+let accountState;
+// accountId = 2 (not allowed to change since defined by let keyword)
+
+accountEmail = "hc@hc.com"
+accountPassword = "23431"
+accountCity = "Delhi"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.log(accountState);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS Code as newer version.
+
+//alert() // we are using nodejs not browser
+
+console.log(3+3);
+
+let name = 3
+let age = 18 
+let isLoggedIn = false 
+
+// number => 2 to power 53 
+// Bigint
+// string
+// boolean => true/false 
+// null => standalone value 
+// undefined 
+// symbol => unique 
+// object 
+
+console.log(typeof "jaskaran")
+console.log(typeof null); // object
+console.log(typeof undefined)
+
